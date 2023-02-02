d244e9d2d46c9c5c2522d729da707a7c5dbf840a
BubbsTheSupreme <trenton.mason14@outlook.com>
2023-02-01 19:27:28 -0500

    Can read from file, fixed a bug with terminal text positioning, and will now prompt before starting

--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@
 namespace RSVPReader;
 class Program
 {
-    static void Main(string[] args)
-    {
+	static void Main(string[] args)
+	{
 		double wpm = Convert.ToDouble(args[0]);
 		string text = args[1];
-        RSVPReader.Rsvp.Read(wpm, text);
-    }
+		// RSVPReader.Rsvp.Read(wpm, text);
+		RSVPReader.Rsvp.ReadFromFile(wpm, text);
+	}
 }
--- Rsvp.cs
+++ Rsvp.cs
@@ -12,13 +12,24 @@ namespace RSVPReader
 			Console.CursorVisible = false;
 			double sec = (1.0 / (wordsPerMinute / 60.0));
 			long wpm = ((long)(sec * 1000));
+			Console.WriteLine("Press Enter to start reading.");
+			Console.ReadLine();
+			Console.Clear();			
 			foreach (string word in text.Split(' '))
 			{
-				Console.Clear();
-				Console.WriteLine($"{word}");
+				Console.Write($"\r{ new string(' ', Console.WindowWidth - 1)} \r");
+				Console.Write($"{new string(' ', Console.WindowWidth / 2)}{word}");
 				Thread.Sleep((int)wpm);
 			}
+			Console.Clear();
 			Console.CursorVisible = true;
 		}
+
+		public static void ReadFromFile(double wordsPerMinute, string filePath)
+		{
+			string text = File.ReadAllText(filePath);
+			Read(wordsPerMinute, text);
+		}
+
 	}
 }
